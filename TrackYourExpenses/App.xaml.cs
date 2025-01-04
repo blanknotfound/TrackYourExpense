5b3e07d6edd34ec2b8f9983cf34c240e2bca823d
Samarastra Bista <104090741+blanknotfound@users.noreply.github.com>
2025-01-04 08:56:17 +0545

    Program push
    this was done after refactoring the file structure of program

---
+++ TrackYourExpenses/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TrackYourExpenses
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
